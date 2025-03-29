a111f44502dac606b9e3e5032619bfc6095ec6fd
shohag390 <shohag390@users.noreply.github.com>
2025-03-29 17:54:39 +0600

    Tailwind css Add

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1 className='font-bold text-[18px] text-[red]'>Hello World</h1>
-    </div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut necessitatibus ex, molestiae at eum fuga libero cupiditate officiis explicabo facere soluta fugit excepturi repellat ipsam praesentium dolore sequi possimus. Quam!</h1>
+    </div >
   );
 };
 
